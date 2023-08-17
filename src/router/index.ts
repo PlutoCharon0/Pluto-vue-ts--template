b3ed5900230fbee544bd626ccce63f260bd61a5a
Pluto <1830233276@qq.com>
2023-08-17 10:06:25 +0800

    feat: 简单配置404页面及路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/:pathMatch(.*)*',
+      component: () => import('@/views/notFound.vue')
+    }
+  ]
 });
 
 export default router;
